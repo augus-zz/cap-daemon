06592992cdee0c6f1cd5b899ba8b1c04fe3dcc77
zouqilin <mr.zouqilin@gmail.com>
2017-09-24 20:04:28 +0800

    use cd

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,13 +14,14 @@ set :bundle_jobs, 4
 namespace :deploy do
   task :restart_cron do
     on roles(:cron) do
-      within "/home/vagrant/test-demo/current" do
+      #within "/home/vagrant/test-demo/current" do
         execute "ps aux|grep cron_job|awk '{print $2}'|xargs kill -9"
         execute "ps aux|grep ping|awk '{print $2}'|xargs kill -9"
         execute "bash -l -c 'rvm use 2.4.2 do ruby --version'"
-        execute "bash -l -c '(nohup rvm use 2.4.2 do ruby /home/vagrant/test-demo/current/bin/cron_job 2>&1 &) && ps -ef | grep cron_job && sleep 2 && ps -ef | grep cron_job' "
+        # execute "bash -l -c '(nohup rvm use 2.4.2 do ruby /home/vagrant/test-demo/current/bin/cron_job 2>&1 &) && ps -ef | grep cron_job && sleep 2 && ps -ef | grep cron_job' "
+        execute "bash -l -c 'cd /home/vagrant/test-demo/current && (nohup rvm use 2.4.2 do ruby bin/cron_job 2>&1 &) && sleep 2' "
         execute "(nohup ping -i 2 www.baidu.com >> /vagrant/log/cron.log 2>&1 &) && sleep 2"
-      end
+      # end
     end
   end
 
