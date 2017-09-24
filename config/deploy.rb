842cce60f2ef47454eece1026539ab40a76b0ae8
zouqilin <mr.zouqilin@gmail.com>
2017-09-24 18:07:37 +0800

    update cron_job

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :keep_releases, 20
 set :bundle_jobs, 4
 
 namespace :deploy do
-  task :start_cron do
+  task :restart_cron do
     on roles(:cron) do
       execute "/bin/bash -l -c 'cd /home/vagrant/test-demo/current; nohup rvm use 2.2.4 do ruby ./bin/cron_job >/dev/null 2>&1 &'"
     end
