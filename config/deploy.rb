b7bcc76f1a20647ce5119e47585f8a2030b64fc5
zouqilin <mr.zouqilin@gmail.com>
2017-09-24 19:49:49 +0800

    print ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ namespace :deploy do
       within "/home/vagrant/test-demo/current" do
         execute "ps aux|grep cron_job|awk '{print $2}'|xargs kill -9"
         execute "ps aux|grep ping|awk '{print $2}'|xargs kill -9"
+        execute "bash -l -c 'rvm use 2.4.2 do ruby --version'"
         execute "bash -l -c '(nohup rvm use 2.4.2 do ruby bin/cron_job 2>&1 &) && sleep 2' "
         execute "(nohup ping -i 2 www.baidu.com >> /vagrant/log/cron.log 2>&1 &) && sleep 2"
       end
