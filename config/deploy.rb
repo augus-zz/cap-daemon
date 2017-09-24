c2888a323cc217f6bb406b8a050f3edc75d6b799
zouqilin <mr.zouqilin@gmail.com>
2017-09-24 13:56:36 +0800

    init

---
+++ config/deploy.rb
@@ -0,0 +1,18 @@
+# config valid only for current version of Capistrano
+lock "3.9.1"
+
+set :application, 'TestDemo'
+set :repo_url, "http://github.com/zouqilin/cap-daemon"
+
+set :pty, true
+set :log_level, :info
+
+set :linked_dirs, %w{log tmp/pids}
+set :keep_releases, 20
+set :bundle_jobs, 4
+
+namespace :deploy do
+	on roles(:cron) do
+		execute "/bin/bash -l -c 'cd /home/vagrant/test-demo/current; nohup rvm use 2.2.4 do ruby ./bin/cron_job >/dev/null 2>&1 &'"
+	end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,8 @@
+server 'localhost', user: 'vagrant', roles: %w{cron}
+
+set :deploy_to, '/home/vagrant/test-demo'
+set :stage, :staging
+set :branch, ENV['BRANCH'] || 'staging'
+set :default_env, { RACK_ENV: 'staging' }
+
+set ssh_options, { forward_agent: false, port: 2222 }
