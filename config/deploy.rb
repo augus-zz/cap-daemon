6566c57aedcf63c90910f46d2287ac4573e59782
zouqilin <mr.zouqilin@gmail.com>
2017-09-24 19:39:58 +0800

    no io redirect

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ namespace :deploy do
       within "/home/vagrant/test-demo/current" do
         execute "ps aux|grep cron_job|awk '{print $2}'|xargs kill -9"
         execute "ps aux|grep ping|awk '{print $2}'|xargs kill -9"
-        execute "(nohup rvm use 2.4.2 do ruby ./bin/cron_job > /dev/null 2>&1 &) && sleep 2"
+        execute "(nohup rvm use 2.4.2 do ruby ./bin/cron_job 2>&1 &) && sleep 2"
         execute "(nohup ping -i 2 www.baidu.com >> /vagrant/log/cron.log 2>&1 &) && sleep 2"
       end
     end
